d0de69323411137e3eaa6839bc5c81a547595533
Malte Hallgreen <malhal@dtu-fvff80kgq05p.clients.wireless.dtu.dk>
2021-10-15 09:28:40 +0200

    nanopore flag

--- cge/resfinder.py
+++ cge/resfinder.py
@@ -547,6 +547,11 @@ if __name__ == '__main__':
                               dest="threshold",
                               help="Blast threshold for identity",
                               default=0.90)
+    parser.add_argument("-nano", "--nanopore",
+                        action="store_true",
+                        dest="nanopore",
+                        help="If nanopore data is used",
+                        default=False)
     args = parser.parse_args()
 
     ##########################################################################
@@ -637,8 +642,12 @@ if __name__ == '__main__':
                            notes=notes_path)
 
         # if input_fastq2 is None, it is ignored by the kma method.
-        kma_run = finder.kma(inputfile_1=input_fastq1,
-                             inputfile_2=input_fastq2)
+        if args.nanopore:
+            kma_run = finder.kma(inputfile_1=input_fastq1,
+                                 inputfile_2=input_fastq2, kma_add_args='-ont -md 5')
+        else:
+            kma_run = finder.kma(inputfile_1=input_fastq1,
+                                 inputfile_2=input_fastq2)
 
         finder.write_results(out_path=out_path, result=kma_run,
                              res_type=ResFinder.TYPE_KMA)
